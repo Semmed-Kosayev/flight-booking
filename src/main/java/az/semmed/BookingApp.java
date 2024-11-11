49428534c1382205d794c45351b9f1278809cba5
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-11-11 04:55:27 -0800

    first initialization

---
+++ src/main/java/az/semmed/BookingApp.java
@@ -0,0 +1,7 @@
+package az.semmed;
+
+public class BookingApp {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/az/semmed/BookingAppTest.java
@@ -0,0 +1,5 @@
+package az.semmed;
+
+public class BookingAppTest {
+
+}
